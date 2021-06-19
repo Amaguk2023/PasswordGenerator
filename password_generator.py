b63d9c80205833103f6d0c53dd051dd2388a69a6
Amaguk2023 <80964822+Amaguk2023@users.noreply.github.com>
2021-06-19 16:36:49 +0200

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -3,7 +3,7 @@ import string, secrets, sys
 # Zorkol Password Generator (ZPG)
 # PYTHON 3.7
 
-# Welcome Message
+#WELCOME MESSAGE
 print('\n')
 print('''
  ZPGZPGZPGZPGZPG     ZPGZPGZPGZPGZPG        ZPGZPGZPGZPGZPG
@@ -26,96 +26,89 @@ print('''
 print('Welcome to Zorkol Password Generator (ZPG).\n')
                
 
-def zorkol_password_generator(): #Main Function
-
-	request = False 
-	while not request: 
+#PASSWORD CREATION PROMPT
+def create_password():
+	try:
 		
-		try:
-
+		request = False 
+		while not request: 
+		
 			request = input('Would you like to create a new password? (Y/n) ').lower() 
+
 			if (request == 'y'):
-
-				include_punctuation = False
-				while not include_punctuation:
-
-					include_punctuation = input('\nDo you want your password to include punctuation characters? (Y/n) ').lower() 
-
-					if include_punctuation == 'y':
-						with_punctuation() 
-					
-					elif include_punctuation == 'n':
-						without_punctuation()
-						
-					else:
-						print('\nPlease check your spelling, value not accepted.')
-						include_punctuation = False
-
+				punctuation_length_amount()
+				
 			elif (request == 'n'):
 				print('\nGoodbye!\n')
 
 			else: 
 				print('\nPlease check your spelling, value not accepted.\n')
-				if __name__ == '__main__': zorkol_password_generator() 
+				request = False #Sets back request to False and process restarts.
 
-		except KeyboardInterrupt: 
-			print('\nGoodbye!\n')
-			sys.exit()
-			
+	except KeyboardInterrupt: 
+		print('\nGoodbye!\n')
+		sys.exit()
 
-#PASSWORDS WITH PUNCTUATITION CHARACTERS FUNCTIONS
-def with_punctuation(): 
-	password_length_amount_with_punctuation()
 
-def password_length_amount_with_punctuation(): 
+#PUNCTUATION, LENGTH AND AMOUNT
+def punctuation_length_amount():
+
 	try:
-		amount_lst = []
+		
 		password_amount = int(input('\nAmount of passwords > '))
 		password_length = int(input('\nLength of passwords > '))
-		for i in range(password_amount):
-			amount_lst.append(''.join(secrets.choice(list(string.ascii_letters) + list(string.digits) + list(string.punctuation)) for i in range (password_length)))  
-			if len(amount_lst) == password_amount:
-				password_list_creation(amount_lst) 
+		
+		include_punctuation = False
+		while not include_punctuation:
+
+			include_punctuation = input('\nDo you want your password to include punctuation characters? (Y/n) ').lower() 
+			
+			if include_punctuation == 'y':
+				with_without_punctuation(password_amount, password_length, include_punctuation = True)
+					
+			elif include_punctuation == 'n':
+				with_without_punctuation(password_amount, password_length, include_punctuation = False)
+						
+			else:
+				print('\nPlease check your spelling, value not accepted.')
+				include_punctuation = False
 
 	except ValueError: 
 		print('\nPlease enter a valid number.')
-		with_punctuation()
-
-
-#PASSWORDS WITHOUT PUNCTUATITION CHARACTERS FUNCTIONS
-def without_punctuation(): 
-	password_length_amount_without_punctuation()
-
-def password_length_amount_without_punctuation():
-	try:
-		amount_lst = []
-		password_length = int(input('\nLength of passwords > '))
-		password_amount = int(input('\nAmount of passwords > '))
-		for i in range(password_amount):
+		punctuation_length_amount()
 			
-			amount_lst.append(''.join(secrets.choice(list(string.ascii_letters) + list(string.digits)) for i in range (password_length)))
-			if len(amount_lst) == password_amount:
-				password_list_creation(amount_lst)
+
+#PASSWORD LIST CREATION WITH OR WITHOUT PUNCTUATITION 
+def with_without_punctuation(password_amount, password_length, include_punctuation): 
 	
-	except ValueError:
-		print('\nPlease enter a valid number.')
-		without_punctuation()
+	if include_punctuation == True:
+		amount_lst = []
+		for i in range(password_amount):
+			amount_lst.append(''.join(secrets.choice(list(string.ascii_letters) + list(string.digits) + list(string.punctuation)) for i in range (password_length)))
+			
+			if len(amount_lst) == password_amount:
+				password_file_creation(amount_lst)
+
+	else:
+		amount_lst = []
+		for i in range(password_amount):
+			amount_lst.append(''.join(secrets.choice(list(string.ascii_letters) + list(string.digits)) for i in range (password_length)))
+			
+			if len(amount_lst) == password_amount:
+				password_file_creation(amount_lst)
 
 
-#PASSWORD FILE CREATION
-def password_list_creation(amount_lst):
+#PASSWORD FILE CREATION AND FINAL MESSAGE
+def password_file_creation(amount_lst):
 	with open('Password_List.txt', 'w') as pl: 
 		pl.write('\n'.join(amount_lst)) 
-		pl.close()
-		exit_message()
+		print('\nPassword_List.txt has been exported.') 
+		print('\nThank you for using ZPG!\n') 
 
-
-#FINAL MESSAGE
-def exit_message():
-	print('\nPassword_List.txt has been exported.') 
-	print('\nThank you for using ZPG!\n') 		
 	
-zorkol_password_generator()
+
+if __name__ == '__main__': 
+	create_password()
 
 
 
